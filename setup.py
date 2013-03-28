ae38c8764f7fe7db70dcd377ba430f8c7195d5d7
Shawn Milochik <shawn@milochik.com>
2013-03-28 11:17:21 -0400

    fixes addition of redis_utils

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='milo',
-    version='0.1.2',
+    version='0.1.3',
     description="Useful Python code, primarily intented for the author's use.",
-    packages=['milo',],
+    packages=['milo', 'milo.redis_utils'],
     license='BSD',
     url='https://github.com/ShawnMilo/milo',
     author='Shawn Milochik',
